e7de35c86d1629f8dfe52f4f5b960168386806b9
maxravel <maxmaterniak@gmail.com>
2018-06-25 12:58:30 +0200

    loging task text in console

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("frwerw");
+
+const submitButton = document.querySelector("#submitB");
+const taskText = document.querySelector("#taskT");
+
+submitButton.addEventListener("click",logTask);
+
+function logTask(){
+    console.log(taskText.value);
+}
